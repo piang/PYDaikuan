f7ab79619042a25169c8425ef6f98c12035f723f
peiyang <peiyang@9fbank.cc>
2016-08-13 18:04:45 +0800

    提交项目

---
+++ PYDaikuan/PYDaikuan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PYDaikuan
+//
+//  Created by 洋 裴 on 16/8/13.
+//  Copyright © 2016年 piang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PYDaikuan/PYDaikuan/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PYDaikuan
+//
+//  Created by 洋 裴 on 16/8/13.
+//  Copyright © 2016年 piang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
