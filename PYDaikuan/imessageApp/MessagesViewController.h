141afc643c90a7ca62edc7f2a2bebe7d1bd9117d
piang <yangpei@sohu-inc.com>
2016-12-28 23:03:02 +0800

    添加 imessage

---
+++ PYDaikuan/imessageApp/MessagesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessagesViewController.h
+//  imessageApp
+//
+//  Created by piang on 16/12/27.
+//  Copyright © 2016年 piang. All rights reserved.
+//
+
+#import <Messages/Messages.h>
+
+@interface MessagesViewController : MSMessagesAppViewController
+
+@end
