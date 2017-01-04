fef2b77bc3912f34d37421f21a026a8c4953ba37
piang <yangpei@sohu-inc.com>
2017-01-04 22:57:11 +0800

    钱袋小额贷

--- PYDaikuan/PYDaikuan/AppDelegate.m
+++ PYDaikuan/PYDaikuan/AppDelegate.m
@@ -38,7 +38,7 @@ bool onlineSetting = false;
         AVQuery *query = [AVQuery queryWithClassName:@"channel_switch"];
         
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-            [query getObjectInBackgroundWithId:@"57cce2bca22b9d006ba3c5a0" block:^(AVObject *object, NSError *error) {
+            [query getObjectInBackgroundWithId:@"586cfa15128fe1006b039f04" block:^(AVObject *object, NSError *error) {
                 NSLog(@"object%@",object);
                 if ([object[@"is_open"] boolValue]) {
                     onlineSetting = true;
