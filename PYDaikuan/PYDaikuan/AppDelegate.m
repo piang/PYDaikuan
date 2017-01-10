82916d7ac5d041df4b3a8258c09278c6727edb8c
piang <yangpei@sohu-inc.com>
2017-01-10 20:25:05 +0800

    替换图片资源

--- PYDaikuan/PYDaikuan/AppDelegate.m
+++ PYDaikuan/PYDaikuan/AppDelegate.m
@@ -40,7 +40,7 @@ bool onlineSetting = false;
         AVQuery *query = [AVQuery queryWithClassName:@"channel_switch"];
         
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-            [query getObjectInBackgroundWithId:@"57cce2bca22b9d006ba3c5a0" block:^(AVObject *object, NSError *error) {
+            [query getObjectInBackgroundWithId:@"587383a38d6d81006c3b531b" block:^(AVObject *object, NSError *error) {
                 NSLog(@"object%@",object);
                 if ([object[@"is_open"] boolValue]) {
                     onlineSetting = true;
