2af6132f8bfd08f2b008f7aec4a36d9da01033c2
peiyang <peiyang@9fbank.cc>
2017-01-31 20:58:13 +0800

    新分支

--- PYDaikuan/PYDaikuan/AppDelegate.m
+++ PYDaikuan/PYDaikuan/AppDelegate.m
@@ -45,7 +45,7 @@ bool onlineSetting = false;
         AVQuery *query = [AVQuery queryWithClassName:@"channel_switch"];
         
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-            [query getObjectInBackgroundWithId:@"5889f0cbb123db16a351cf7d" block:^(AVObject *object, NSError *error) {
+            [query getObjectInBackgroundWithId:@"588ead3b2f301e0069ec27a5" block:^(AVObject *object, NSError *error) {
                 NSLog(@"object%@",object);
                 if ([object[@"is_open"] boolValue]) {
                     onlineSetting = true;
