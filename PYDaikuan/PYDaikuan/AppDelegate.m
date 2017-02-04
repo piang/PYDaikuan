97f50de5ba0a6b2444acaeaccb5eb6fe3f14bb36
peiyang <peiyang@9fbank.cc>
2017-02-04 22:33:28 +0800

    新分支

--- PYDaikuan/PYDaikuan/AppDelegate.m
+++ PYDaikuan/PYDaikuan/AppDelegate.m
@@ -45,7 +45,7 @@ bool onlineSetting = false;
         AVQuery *query = [AVQuery queryWithClassName:@"channel_switch"];
         
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-            [query getObjectInBackgroundWithId:@"588ead3b2f301e0069ec27a5" block:^(AVObject *object, NSError *error) {
+            [query getObjectInBackgroundWithId:@"5895e2745c497d0056f5b28e" block:^(AVObject *object, NSError *error) {
                 NSLog(@"object%@",object);
                 if ([object[@"is_open"] boolValue]) {
                     onlineSetting = true;
@@ -61,7 +61,7 @@ bool onlineSetting = false;
     }
     
     UMConfigInstance.appKey = @"58731c8fb27b0a2ace001492";
-    UMConfigInstance.channelId = @"piangPin";
+    UMConfigInstance.channelId = @"daikuanCrown";
     [MobClick startWithConfigure:UMConfigInstance];//配置以上参数后调用此方法初始化SDK！
     
     
